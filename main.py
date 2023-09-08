f67dd5b50868b2b5a1ffbc4b300fd48631e41479
Sam Black <sam.black@lubbockisd.org>
2023-09-08 14:47:40 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,70 @@
+print("CompSci", end = " ")
+print("Rocks!")
+
+# demoing escape characters
+print("CompSci \\Rocks!")
+# print("CompSci \tRocks!")
+print("CompSci \nRocks!")
+print("Robert \"Bobby\" Smith")
+
+# variables
+grade = 86
+student = "Bob"
+theEnd = False  #camel case
+
+asdfghkl = 47  #bad choice
+name = "john"
+Name = "Susan"
+NAME = "Jane"
+
+print(Name)
+
+# data types
+# boolean -- True or False
+# integer -- whole numbers
+# float -- decimal numbers
+# string -- words, can include numbers, phrases, things between " "
+# list  -- a series of variables:  ex. [23, "Hi", False]
+# tuple -- an unchangeable list: ex. (23, "Hi", False)
+
+print("their name is", name)
+
+numString = "56"
+num = int(numString)
+print(4 + num)
+"""
+# adding interactivity -- INPUT!
+userName = input("What's your name? ")
+print("Nice to meet you,",userName)
+numVal = int(input("What's your favorite number? "))
+print(numVal, "is a lucky number")
+decVal = float(input("Give me a real number:"))
+print(decVal)
+"""
+# Math Ops
+# total = apples + oranges
+# price = quantity * priceEach
+# +, -, *
+#  division -- / and //
+# remainder -- %
+# exponents **    ex. 5**2 -> 25
+print("Total is", str(3+4))
+print("Product is", str(1.5*12))
+intDiv = 3 // 4  #integer division
+print("integer division is",intDiv)
+decDiv = 3 / 4.0   #real division
+print("decimal division is",decDiv)
+# remainders --> %
+print(10 % 2)
+print(9 % 2)
+print(7 % 3)
+print(3 % 7)
+
+# shortcuts
+num = num + 10
+num += 10
+
+num *= 5
+
+
+
